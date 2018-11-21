f09db3015c394d53d9079d08fb0de63bb614a9ca
Jake Ma <37993293+JakeMa2017@users.noreply.github.com>
2018-11-20 23:23:16 -0600

    Added 415

---
+++ Two Pointers/415. Valid Parlindrome/README.md
@@ -0,0 +1,12 @@
+# Valid Palindrome
+
+Given a string, determine if it is a palindrome, considering only alphanumeric characters and ignoring cases.
+
+Example
+
+`"A man, a plan, a canal: Panama"` is a palindrome.
+
+`"race a car"` is not a palindrome.
+
+---
+Use Character class to check if it's a character or digit and since it's case insensitive, cast all letters to upper(lower) case.
---
+++ Two Pointers/415. Valid Parlindrome/ValidParlindrome.java
@@ -0,0 +1,43 @@
+/**
+ * Valid Parlindrome
+ * Two Pointers
+ * Jake Ma
+ * 11-20-2018
+ */
+
+public class ValidParlindrome {
+    /**
+     * @param s: A string
+     * @return: Whether the string is a valid palindrome
+     */
+    public boolean isPalindrome(String s) {
+        if (s.length() == 0 || s == null) {
+            return true;
+        }
+        
+        int left = 0;
+        int right = s.length() - 1;
+        
+        while (left < right) {
+            while (!isValid(s.charAt(left)) && left < right) {
+                left++;
+            }
+            while (!isValid(s.charAt(right)) && left < right) {
+                right--;
+            }
+            
+            if (Character.toUpperCase(s.charAt(left)) != Character.toUpperCase(s.charAt(right))) {
+                return false;
+            } else {
+                left++;
+                right--;
+            }
+        }
+        
+        return true;
+    }
+    
+    private boolean isValid(char c) {
+        return Character.isLetter(c) || Character.isDigit(c);
+    }
+}
