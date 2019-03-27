7bfeeee93edf9112d7d631773bb7db841a9b647d
Jake Ma <37993293+JakeMa2017@users.noreply.github.com>
2019-03-26 22:07:40 -0500

    Added 242

---
+++ BFS/242. Convert Binary Tree to Linked Lists/ConvertBinaryTreeToLinkedListsByDepth.java
@@ -0,0 +1,62 @@
+/**
+ * Jake Ma
+ * 3/26/2019
+ * BFS
+ * 
+ * Definition of TreeNode:
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left, right;
+ *     public TreeNode(int val) {
+ *         this.val = val;
+ *         this.left = this.right = null;
+ *     }
+ * }
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+public class ConvertBinaryTreeToLinkedListsByDepth {
+    /**
+     * @param root the root of binary tree
+     * @return a lists of linked list
+     */
+    public List<ListNode> binaryTreeToLists(TreeNode root) {
+        List<ListNode> result = new ArrayList<>();
+        
+        if (root == null) {
+            return result;
+        }
+        
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        
+        ListNode dummy = new ListNode(0);
+        ListNode lastNode = null;
+        while (!queue.isEmpty()) {
+            dummy.next = null;
+            lastNode = dummy;
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode head = queue.poll();
+                lastNode.next = new ListNode(head.val);
+                lastNode = lastNode.next;
+                
+                if (head.left != null) {
+                    queue.offer(head.left);
+                }
+                if (head.right != null) {
+                    queue.offer(head.right);
+                }
+            }
+            result.add(dummy.next);
+        }
+        
+        return result;
+        
+    }
+    
+}
---
+++ BFS/242. Convert Binary Tree to Linked Lists/README.md
@@ -0,0 +1,42 @@
+#Convert Binary Tree to Linked Lists by Depth
+
+Given a binary tree, design an algorithm which creates a linked list of all the nodes at each depth (e.g., if you have a tree with depth D, you'll have D linked lists).
+
+Example 1:
+
+Input:
+```
+{1,2,3,4}
+    1
+   / \
+  2   3
+ /
+4
+```
+Output:
+```
+[
+  1->null,
+  2->3->null,
+  4->null
+]
+```
+Example 2:
+
+Input:
+```
+{1,#,2,3}
+ 1
+ \
+  2
+ /
+3
+```
+Output:
+```
+[
+  1->null,
+  2->null,
+  3->null
+]
+```
