ab579f675f7b6da0cfa60b73a93f7d6827088b7c
Jake Ma <37993293+JakeMa2017@users.noreply.github.com>
2019-04-03 16:24:20 -0500

    Update 547 for two more solutions

--- Data Structure/547. Intersection of Two Arrays/IntersectionOfTwoArrays.java
+++ Data Structure/547. Intersection of Two Arrays/IntersectionOfTwoArrays.java
@@ -12,12 +12,22 @@ public class Solution {
      * @return: an integer array
      */
     public int[] intersection(int[] nums1, int[] nums2) {
-
-        return method1(nums1, nums2);
+        
+        //return method1(nums1, nums2);
+        //return method2(nums1, nums2);
+        return method3(nums1, nums2);
+        
     }
     
     // sort and merge
     private int[] method1(int[] nums1, int[] nums2) {
+        if (nums1 == null || nums2 == null) {
+            return null;
+        }
+        if (nums1.length == 0 || nums2.length == 0) {
+            return new int[0];
+        }
+        
         Arrays.sort(nums1);
         Arrays.sort(nums2);
         
@@ -45,5 +55,90 @@ public class Solution {
         }
         
         return result;
+    }
+    
+    // HashSet
+    private int[] method2(int[] nums1, int[] nums2) {
+        if (nums1 == null || nums2 == null) {
+            return null;
+        }
+        if (nums1.length == 0 || nums2.length == 0) {
+            return new int[0];
+        }
+        
+        HashSet<Integer> init = new HashSet<>();
+        for (int i = 0; i < nums1.length; i++) {
+            init.add(nums1[i]);
+        }
+        
+        HashSet<Integer> resultHash = new HashSet<>();
+        for (int i = 0; i < nums2.length; i++) {
+            if (init.contains(nums2[i]) && !resultHash.contains(nums2[i])) {
+                resultHash.add(nums2[i]);
+            }
+        }
+        
+        int[] result = new int[resultHash.size()];
+        int index = 0;
+        for (int n : resultHash) {
+            result[index++] = n;
+        }
+        
+        return result;
+    }
+    
+    // HashSet and BS
+    private int[] method3(int[] nums1, int[] nums2) {
+        if (nums1 == null || nums2 == null) {
+            return null;
+        }
+        if (nums1.length == 0 || nums2.length == 0) {
+            return new int[0];
+        }
+        
+        HashSet<Integer> set = new HashSet<>();
+        Arrays.sort(nums1);
+        
+        // add intersected number into the HashSet
+        for (int i = 0; i < nums2.length; i++) {
+            if (set.contains(nums2[i])) {
+                continue;
+            }
+            if (binarySearch(nums1, nums2[i])) {
+                set.add(nums2[i]);
+            }
+        }
+        
+        int[] result = new int[set.size()];
+        int index = 0;
+        for (int n : set) {
+            result[index++] = n;
+        }
+        
+        return result;
+    }
+    
+    private boolean binarySearch(int[] nums1, int n) {
+        int start = 0;
+        int end = nums1.length - 1;
+        
+        while (start + 1 < end) {
+            int mid = start + (end - start) / 2;
+            if (n == nums1[mid]) {
+                return true;
+            }
+            
+            if (n < nums1[mid]) {
+                end = mid;
+            } else {
+                start = mid;
+            }
+        }
+        
+        if (n == nums1[start] || n == nums1[end]) {
+            return true;
+        }
+        
+        return false;
     }
 }
