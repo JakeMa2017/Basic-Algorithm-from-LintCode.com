449863140665e08e65b856c02e04e2c2213e2468
Jake Ma <37993293+JakeMa2017@users.noreply.github.com>
2019-04-08 15:51:35 -0500

    Added 902

---
+++ Binary Tree/902. Kth Samllest Element in a BST/KthSmallestElementInABST.java
@@ -0,0 +1,50 @@
+/**
+ * Jake Ma
+ * Kth Smallest Element in a Binary Tree
+ * BST
+ * 4/8/2019
+ * 
+ * Definition of TreeNode:
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left, right;
+ *     public TreeNode(int val) {
+ *         this.val = val;
+ *         this.left = this.right = null;
+ *     }
+ * }
+ */
+
+public class KthSmallestElementInABST {
+    /**
+     * @param root: the given BST
+     * @param k: the given k
+     * @return: the kth smallest element in BST
+     */
+    private int result = 0;
+    private int count = 0;
+    
+    public int kthSmallest(TreeNode root, int k) {
+        if (root == null) {
+            return result;
+        }
+        helper(root, k);
+        return result;
+    }
+    
+    // in order traversal
+    private void helper(TreeNode root, int k) {
+        if (root == null) {
+            return;
+        }
+        
+        helper(root.left, k);
+        count++;
+        if (count == k) {
+            result = root.val;
+            return;
+        }
+
+        helper(root.right, k);
+    }
+}
---
+++ Binary Tree/902. Kth Samllest Element in a BST/README.md
@@ -0,0 +1,10 @@
+# Kth Smallest Element in a BST
+
+Given a binary search tree, write a function kthSmallest to find the kth smallest element in it.
+
+### Example  
+Given `root = {1,#,2}`, `k = 2`, return `2`.
+
+
+### Notice
+You may assume `k` is always valid, `1` ≤ `k` ≤ BST's total elements.
