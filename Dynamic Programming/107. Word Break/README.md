7590cd75c1ea7258f4ab56e975789e1d170686bd
Jake Ma <37993293+JakeMa2017@users.noreply.github.com>
2019-04-20 16:24:54 -0500

    Added 107

---
+++ Dynamic Programming/107. Word Break/README.md
@@ -0,0 +1,13 @@
+# Word Break
+
+Given a string `s` and a dictionary of words dict, determine if `s` can be break into a space-separated sequence of one or more dictionary words.
+
+
+### Example 1
+
+Input:  `"lintcode"`, `["lint", "code"]`  
+Output:  `true`
+
+### Example 2
+Input: `"a"`, `["a"]`  
+Output:  `true`
---
+++ Dynamic Programming/107. Word Break/WordBreak.java
@@ -0,0 +1,32 @@
+public class WordBreak {
+    /*
+     * @param s: A string
+     * @param dict: A dictionary of words dict
+     * @return: A boolean
+     */
+    public boolean wordBreak(String s, Set<String> dict) {
+       
+       // find the length of the longest word in dictionary
+       int maxLength = 0;
+       for (String str : dict) {
+           maxLength = Math.max(maxLength, str.length());
+       }
+       
+       // there are n + 1 break points in one string, like -h-a-r-d-
+       boolean[] canBreak = new boolean[s.length() + 1];
+       canBreak[0] = true;
+       
+       for (int i = 1; i < canBreak.length; i++) {
+           for (int j = 1; j <= maxLength && j <= i; j++) {
+               // i - j here means the point which starts at j and ends at i
+               String cur = s.substring(i - j, i);
+               if (dict.contains(cur) && canBreak[i - j]) {
+                   canBreak[i] = true;
+                   break;
+               }
+           }
+       }
+       
+       return canBreak[canBreak.length - 1];
+    }
+}
